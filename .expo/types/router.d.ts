a8332e42483d8a78be17595766d2ff58e62f447a
alexandriapulham <alexandriapulham@gmail.com>
2024-10-16 11:16:30 -0400

    imported ingredients, updated ingredient file to include ids (also changed tag to dietTag for clarity), updated recipe file to include dietTags and ingredTags for easy searching

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/recipes` | `/(tabs)/tabthree` | `/_sitemap` | `/explore` | `/recipes` | `/tabthree`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/(tabs)/recipes` | `/(tabs)/tabthree` | `/..\components\IngredientImport` | `/..\components\RecipeImport` | `/_sitemap` | `/explore` | `/recipes` | `/tabthree`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
--- components/PantryIngredients.tsx
+++ components/PantryIngredients.tsx
@@ -1,39 +1,25 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Nutrition } from "./Recipe";
-
-// For now created a different class for ingredients in the pantry to decide whether we want to have
-// two classes or change the json ingredents objects
-export class PantryIngredient {
-  name: string;
-  tags: Number[];
-  macros: Nutrition;
-
-  public constructor(name: string, tags: Number[], macros: Nutrition) {
-    this.name = name;
-    this.tags = tags;
-    this.macros = macros;
-  }
-}
+import { Ingredient } from "./Ingredient";
 
 // This function generate a json file from a list of pantry infredients
-export const savePantryIngredients = async (ingredients: PantryIngredient[]) => {
-	try {
-	  const jsonString = JSON.stringify(ingredients, null, 2); 
-	  await AsyncStorage.setItem('pantryIngredients', jsonString);
-	  console.log('Ingredients saved successfully.');
-	} catch (error) {
-	  console.error('Error saving ingredients:', error);
-	}
+export const savePantryIngredients = async (ingredients: Ingredient[]) => {
+  try {
+    const jsonString = JSON.stringify(ingredients, null, 2);
+    await AsyncStorage.setItem("pantryIngredients", jsonString);
+    console.log("Ingredients saved successfully.");
+  } catch (error) {
+    console.error("Error saving ingredients:", error);
+  }
 };
 
 // This function returns an object list of pantryIngredients from the json
-export const loadPantryIngredients = async (): Promise<PantryIngredient[] | null> => {
-	try {
-	  const jsonString = await AsyncStorage.getItem('pantryIngredients');
-	  if (!jsonString) return null; 
-	  return JSON.parse(jsonString); 
-	} catch (error) {
-	  console.error('Error loading ingredients:', error);
-	  return null;
-	}
-  };
+export const loadPantryIngredients = async (): Promise<Ingredient[] | null> => {
+  try {
+    const jsonString = await AsyncStorage.getItem("pantryIngredients");
+    if (!jsonString) return null;
+    return JSON.parse(jsonString);
+  } catch (error) {
+    console.error("Error loading ingredients:", error);
+    return null;
+  }
+};
