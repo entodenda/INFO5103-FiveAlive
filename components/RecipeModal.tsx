37379e23305f2cfc9043d6b9135d2b803a939501
Tradicans <54860251+Tradicans@users.noreply.github.com>
2024-11-25 13:48:10 -0500

    WIP - updating recipe modal to match design

--- components/RecipeModal.tsx
+++ components/RecipeModal.tsx
@@ -14,6 +14,8 @@ import {
 import { ConvertMinsToHours } from "./RecipeImport";
 import Fraction from "fraction.js";
 import { ChangeInfoScale } from "./Scale";
+import { StarRatingDisplay } from "react-native-star-rating-widget";
+import { ImageSourcePropType } from "react-native";
 
 import { Recipe, Serving } from "./Recipe";
 import Toast from "react-native-toast-message";
@@ -29,12 +31,26 @@ const FractionDisplay: React.FC<{ value: number }> = ({ value }) => {
   return <Text>{fraction.toFraction(true)}</Text>;
 };
 
+const iconSourceFiles: { [key: number]: ImageSourcePropType } = {
+  1: require("../assets/images/glutenfree.png"),
+  2: require("../assets/images/veganpro.png"),
+  3: require("../assets/images/vegetarian.png"),
+  4: require("../assets/images/dairyfree.png"),
+  5: require("../assets/images/nutfree.png"),
+};
+
+const renderIcon = (tag: number) => {
+  const imageSource = iconSourceFiles[tag];
+  return <Image key={tag} style={styles.tinyLogo} source={imageSource} />;
+};
+
 type SectionKey =
   | "source"
   | "cookingTimes"
   | "nutrition"
   | "ingredients"
-  | "instructions";
+  | "instructions"
+  | "primaryInfo";
 
 const RecipeModal: React.FC<RecipeModalProps> = ({
   visible,
@@ -46,7 +62,8 @@ const RecipeModal: React.FC<RecipeModalProps> = ({
     cookingTimes: false,
     nutrition: false,
     ingredients: false,
-    instructions: true,
+    instructions: false,
+    primaryInfo: true,
   };
 
   const [expandedSections, setExpandedSections] = useState(
@@ -109,107 +126,136 @@ const RecipeModal: React.FC<RecipeModalProps> = ({
                 <Ionicons name="close" size={24} color="black" />
               </Pressable>
             </View>
+            <View style={styles.headerContainer}>
+              <StarRatingDisplay
+                rating={parseFloat(recipe.rating)}
+                color="#f7931e"
+                starSize={20}
+              />
+              <Text style={styles.ratingText}>{recipe.rating}</Text>
+              <View style={styles.tagContainer}>
+                {Object.entries(iconSourceFiles).map(([key]) => {
+                  const tag = Number(key);
+                  return !recipe.dietTag?.includes(tag)
+                    ? renderIcon(tag)
+                    : null;
+                })}
+              </View>
+            </View>
+
             <ScrollView
               contentContainerStyle={styles.scrollViewContent}
               showsVerticalScrollIndicator={false}
               showsHorizontalScrollIndicator={false}
             >
-              {/* Instructions Section */}
+              {/* Primary Info Section */}
               <Pressable
-                onPress={() => toggleSection("instructions")}
+                // onPress={() => toggleSection("primaryInfo")}
                 style={styles.card}
               >
-                <Text style={styles.sectionTitle}>Instructions:</Text>
-                {expandedSections.instructions && (
+                {expandedSections.primaryInfo && (
                   <>
-                    {recipe.instructions.map((step, index) => (
-                      <Text key={index} style={styles.sectionText}>{`${
-                        index + 1
-                      }. ${step}`}</Text>
-                    ))}
-                  </>
-                )}
-              </Pressable>
+                    <View>
+                      <View style={styles.row}>
+                        <View style={styles.column}>
+                          {recipe.time.bakeTime != null && (
+                            <View style={styles.row}>
+                              <Text style={styles.sectionText}>
+                                Bake Time:{" "}
+                                {ConvertMinsToHours(recipe.time.bakeTime)}
+                              </Text>
+                            </View>
+                          )}
 
-              {/* Cooking Times Section */}
-              <Pressable
-                onPress={() => toggleSection("cookingTimes")}
-                style={styles.card}
-              >
-                <Text style={styles.sectionTitle}>Cooking Times:</Text>
-                {expandedSections.cookingTimes && (
-                  <>
-                    {recipe.time.bakeTime != null && (
-                      <Text style={styles.sectionText}>
-                        Bake time: {ConvertMinsToHours(recipe.time.bakeTime)}
-                      </Text>
-                    )}
-                    {recipe.time.cookTime != null && (
-                      <Text style={styles.sectionText}>
-                        Cook time: {ConvertMinsToHours(recipe.time.cookTime)}
-                      </Text>
-                    )}
-                    {recipe.time.prepTime != null && (
-                      <Text style={styles.sectionText}>
-                        Prep time: {ConvertMinsToHours(recipe.time.prepTime)}
-                      </Text>
-                    )}
-                    {recipe.time.totalTime != null && (
-                      <Text style={styles.sectionText}>
-                        Total time: {ConvertMinsToHours(recipe.time.totalTime)}
-                      </Text>
-                    )}
-                    <View style={{ flexDirection: "row" }}>
-                      <Text style={styles.sectionText}>Servings: </Text>
-                      <TextInput
-                        style={styles.inputText}
-                        keyboardType="numeric"
-                        value={String(servings)}
-                        onChangeText={onScaleChange}
-                        placeholder={String(recipe.serving.servings)}
-                      />
+                          {recipe.time.cookTime != null && (
+                            <View style={styles.row}>
+                              <Text style={styles.sectionText}>
+                                Cook Time:{" "}
+                                {ConvertMinsToHours(recipe.time.cookTime)}
+                              </Text>
+                            </View>
+                          )}
+                          {recipe.time.prepTime != null && (
+                            <View style={styles.row}>
+                              <Text style={styles.sectionText}>
+                                Prep Time:{" "}
+                                {ConvertMinsToHours(recipe.time.prepTime)}
+                              </Text>
+                            </View>
+                          )}
+                          {recipe.time.totalTime != null && (
+                            <View style={styles.row}>
+                              <Text style={styles.sectionText}>
+                                Total Time:{" "}
+                                {ConvertMinsToHours(recipe.time.totalTime)}
+                              </Text>
+                            </View>
+                          )}
+                          <View style={styles.row}>
+                            <Text style={styles.sectionText}>Serves: </Text>
+                            <TextInput
+                              style={styles.inputText}
+                              keyboardType="numeric"
+                              value={String(servings)}
+                              onChangeText={onScaleChange}
+                              placeholder={String(recipe.serving.servings)}
+                            />
+                          </View>
+
+                          <View style={styles.nutritionContainer}>
+                            <View style={styles.row}>
+                              <View style={styles.column}>
+                                <Text style={styles.servingTxt}>
+                                  Per Serving:
+                                </Text>
+                                <View>
+                                  <View style={styles.row}>
+                                    <View style={styles.column}>
+                                      <Text style={styles.listTxt}>
+                                        •{" "}
+                                        {recipe.nutrition.calories === null
+                                          ? "N/A"
+                                          : recipe.nutrition.calories}{" "}
+                                        Calories
+                                      </Text>
+                                    </View>
+                                    <View style={styles.column}>
+                                      <Text style={styles.listTxt}>
+                                        •{" "}
+                                        {recipe.nutrition.fat === null
+                                          ? "N/A"
+                                          : recipe.nutrition.fat}
+                                        g Fat
+                                      </Text>
+                                    </View>
+                                  </View>
+                                  <View style={styles.row}>
+                                    <View style={styles.column}>
+                                      <Text style={styles.listTxt}>
+                                        •{" "}
+                                        {recipe.nutrition.protein === null
+                                          ? "N/A"
+                                          : recipe.nutrition.protein}
+                                        g Protein
+                                      </Text>
+                                    </View>
+                                    <View style={styles.column}>
+                                      <Text style={styles.listTxt}>
+                                        •{" "}
+                                        {recipe.nutrition.carbs === null
+                                          ? "N/A"
+                                          : recipe.nutrition.carbs}
+                                        g Carbs
+                                      </Text>
+                                    </View>
+                                  </View>
+                                </View>
+                              </View>
+                            </View>
+                          </View>
+                        </View>
+                      </View>
                     </View>
-                  </>
-                )}
-              </Pressable>
-
-              {/* Nutritional Information Section */}
-              <Pressable
-                onPress={() => toggleSection("nutrition")}
-                style={styles.card}
-              >
-                <Text style={styles.sectionTitle}>
-                  Nutritional Information:
-                </Text>
-                {expandedSections.nutrition && (
-                  <>
-                    <Text style={styles.sectionText}>
-                      Calories:{" "}
-                      {recipe.nutrition.calories === null
-                        ? "N/A"
-                        : recipe.nutrition.calories}
-                    </Text>
-                    <Text style={styles.sectionText}>
-                      {`Carbs: ${
-                        recipe.nutrition.carbs === null
-                          ? "N/A"
-                          : recipe.nutrition.carbs + "g"
-                      }`}
-                    </Text>
-                    <Text style={styles.sectionText}>
-                      {`Fat: ${
-                        recipe.nutrition.fat === null
-                          ? "N/A"
-                          : recipe.nutrition.fat + "g"
-                      }`}
-                    </Text>
-                    <Text style={styles.sectionText}>
-                      {`Protein: ${
-                        recipe.nutrition.protein === null
-                          ? "N/A"
-                          : recipe.nutrition.protein + "g"
-                      }`}
-                    </Text>
                   </>
                 )}
               </Pressable>
@@ -242,6 +288,24 @@ const RecipeModal: React.FC<RecipeModalProps> = ({
                   </>
                 )}
               </Pressable>
+
+              {/* Instructions Section */}
+              <Pressable
+                onPress={() => toggleSection("instructions")}
+                style={styles.card}
+              >
+                <Text style={styles.sectionTitle}>Instructions:</Text>
+                {expandedSections.instructions && (
+                  <>
+                    {recipe.instructions.map((step, index) => (
+                      <Text key={index} style={styles.sectionText}>{`${
+                        index + 1
+                      }. ${step}`}</Text>
+                    ))}
+                  </>
+                )}
+              </Pressable>
+
               {/* Source Section */}
               <Pressable
                 onPress={() => toggleSection("source")}
@@ -313,7 +377,7 @@ const styles = StyleSheet.create({
     alignSelf: "flex-end",
   },
   modalTitle: {
-    fontSize: 24,
+    fontSize: 30,
     fontWeight: "bold",
     paddingRight: 10,
   },
@@ -345,14 +409,50 @@ const styles = StyleSheet.create({
     marginLeft: 10, // Indent for subtext
     marginTop: 2,
   },
+  ratingText: {
+    fontSize: 16,
+    marginLeft: 10, // Indent for subtext
+    //alignSelf: "flex-start",
+    //textAlign: "left",
+  },
   inputText: {
     fontSize: 16,
     marginLeft: 10, // Indent for subtext
-    marginTop: 2,
-    padding: 3,
+    // marginTop: 2,
+    // padding: 3,
+    paddingHorizontal: 5,
     borderWidth: 1,
     borderColor: "gray",
     borderRadius: 5,
+  },
+  tinyLogo: {
+    width: 35,
+    height: 35,
+  },
+  tagContainer: {
+    flexDirection: "row-reverse",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
+  column: {
+    flex: 1,
+  },
+
+  nutritionContainer: {
+    //position: "absolute",
+    //top: 40,
+    //left: 20,
+    width: "100%",
+  },
+  servingTxt: {
+    fontSize: 16,
+    paddingLeft: 10,
+  },
+  listTxt: {
+    fontSize: 16,
+    paddingLeft: 25,
   },
 });
 
