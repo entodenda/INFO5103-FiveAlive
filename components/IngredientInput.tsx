0373010984f1eee0a294c97dd5cab29b4566b24f
Amber Rose <a_rose2@fanshaweonline.ca>
2024-11-26 14:30:05 -0500

    added button to delete ingredients

--- components/IngredientInput.tsx
+++ components/IngredientInput.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import {
-  View,
-  StyleSheet,
-  TextInput,
-  Modal,
-  Text,
-  GestureResponderEvent,
-  ScrollView,
-  Pressable,
+	View,
+	StyleSheet,
+	TextInput,
+	Modal,
+	Text,
+	GestureResponderEvent,
+	ScrollView,
+	Pressable,
 } from "react-native";
 import { Ingredient } from "@/components/Ingredient";
 import { AllIngredientsImport } from "./IngredientImport";
@@ -18,153 +18,164 @@ import { ListItem } from "react-native-elements";
 // todo - reset search string on modal re-open
 
 const IngredientInput = (props: {
-  onAddItem: (ingredient: Ingredient) => void;
-  visible: boolean;
-  onCancel: (event: GestureResponderEvent) => void;
+	onAddItem: (ingredient: Ingredient) => void;
+	visible: boolean;
+	onCancel: (event: GestureResponderEvent) => void;
 }) => {
-  const [enteredIngredientName, setIngredientName] = useState<string>("");
-  const [allIngredients, setAllIngredients] = useState<Ingredient[]>([]);
-  const [matchedIngredients, setMatchedIngredients] = useState<Ingredient[]>(
-    []
-  );
+	const [enteredIngredientName, setIngredientName] = useState<string>("");
+	const [allIngredients, setAllIngredients] = useState<Ingredient[]>([]);
+	const [matchedIngredients, setMatchedIngredients] = useState<Ingredient[]>(
+		[]
+	);
 
-  // to ensure app functionality while loading
-  useEffect(() => {
-    const getIngredients = async () => {
-      try {
-        const ingredImport: Ingredient[] = await AllIngredientsImport();
-        setAllIngredients(ingredImport);
-      } catch (error) {
-        console.error("Error fetching recipes", error);
-      }
-    };
-    getIngredients();
-  }, []);
+	// to ensure app functionality while loading
+	useEffect(() => {
+		const getIngredients = async () => {
+			try {
+				const ingredImport: Ingredient[] = await AllIngredientsImport();
+				setAllIngredients(ingredImport);
+			} catch (error) {
+				console.error("Error fetching recipes", error);
+			}
+		};
+		getIngredients();
+	}, []);
 
-  // speeds up ingred search
-  useEffect(() => {
-    const delay = setTimeout(() => {
-      if (enteredIngredientName) {
-        setMatchedIngredients(
-          FindMatchingIngredients(enteredIngredientName, allIngredients)
-        );
-      } else {
-        setMatchedIngredients([]);
-      }
-    }, 300);
-    return () => clearTimeout(delay);
-  }, [enteredIngredientName, allIngredients]);
+	// speeds up ingred search
+	useEffect(() => {
+		const delay = setTimeout(() => {
+			if (enteredIngredientName) {
+				setMatchedIngredients(
+					FindMatchingIngredients(enteredIngredientName, allIngredients)
+				);
+			} else {
+				setMatchedIngredients([]);
+			}
+		}, 300);
+		return () => clearTimeout(delay);
+	}, [enteredIngredientName, allIngredients]);
 
-  const IngredientNameHandler = (name: string) => {
-    setIngredientName(name);
-    //setMatchedIngredients(FindMatchingIngredients(name, allIngredients));
-  };
+	const IngredientNameHandler = (name: string) => {
+		setIngredientName(name);
+		//setMatchedIngredients(FindMatchingIngredients(name, allIngredients));
+	};
 
-  return (
-    <Modal visible={props.visible} animationType="slide">
-      <View style={styles.inputContainer}>
-        <View>
-          <Text style={styles.head}>~ Add Ingredients ~</Text>
-        </View>
-        <View style={styles.buttonContainer}>
-          <Pressable style={styles.button} onPress={props.onCancel}>
-            <Text style={styles.cancelText}>Return to Pantry</Text>
-          </Pressable>
-        </View>
-        <TextInput
-          placeholder="Ingredient Name"
-          style={styles.input}
-          onChangeText={IngredientNameHandler}
-          value={enteredIngredientName}
-        />
+	return (
+		<Modal
+			visible={props.visible}
+			animationType="slide"
+		>
+			<View style={styles.inputContainer}>
+				<View>
+					<Text style={styles.head}>~ Add Ingredients ~</Text>
+				</View>
+				<View style={styles.buttonContainer}>
+					<Pressable
+						style={styles.button}
+						onPress={props.onCancel}
+					>
+						<Text style={styles.cancelText}>Return to Pantry</Text>
+					</Pressable>
+				</View>
+				<TextInput
+					placeholder="Ingredient Name"
+					style={styles.input}
+					onChangeText={IngredientNameHandler}
+					value={enteredIngredientName}
+				/>
 
-        <ScrollView>
-          {matchedIngredients.map((ingredient) => (
-            <ListItem key={ingredient.id}>
-              <View style={styles.widgetContainer}>
-                <IngredientWidget key={ingredient.id} ingredient={ingredient} />
-                <View style={styles.addButtonContainer}>
-                  <Pressable
-                    style={styles.addbutton}
-                    onPress={props.onAddItem.bind(this, ingredient)}
-                  >
-                    <Text style={styles.addtext}>+</Text>
-                  </Pressable>
-                </View>
-              </View>
-            </ListItem>
-          ))}
-        </ScrollView>
-      </View>
-    </Modal>
-  );
+				<ScrollView>
+					{matchedIngredients.map((ingredient) => (
+						<ListItem key={ingredient.id}>
+							<View style={styles.widgetContainer}>
+								<IngredientWidget
+									key={ingredient.id}
+									ingredient={ingredient}
+								/>
+								<View style={styles.addButtonContainer}>
+									<Pressable
+										style={styles.addbutton}
+										onPress={() => {
+											props.onAddItem(ingredient), setIngredientName("");
+										}}
+									>
+										<Text style={styles.addtext}>+</Text>
+									</Pressable>
+								</View>
+							</View>
+						</ListItem>
+					))}
+				</ScrollView>
+			</View>
+		</Modal>
+	);
 };
 
 const styles = StyleSheet.create({
-  inputContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  input: {
-    width: "80%",
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10,
-    marginBottom: 20,
-    borderRadius: 20,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    width: "95%",
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: "#D9D9D9",
-    padding: 3,
-    borderRadius: 20,
-    textAlign: "center",
-  },
-  cancelText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "Black",
-    textAlign: "center",
-  },
-  addtext: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "white",
-  },
-  addbutton: {
-    alignItems: "flex-end",
-    width: 30,
-    height: 30,
-    justifyContent: "center",
-    paddingHorizontal: 9,
-    borderRadius: 50,
-    elevation: 3,
-    backgroundColor: "#1F51FF",
-  },
-  addButtonContainer: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    width: "100%",
-    marginTop: -50,
-  },
-  head: {
-    marginTop: 30,
-    marginBottom: 20,
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "black",
-  },
-  widgetContainer: {
-    marginBottom: 10,
-    paddingBottom: 10,
-  },
+	inputContainer: {
+		flex: 1,
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	input: {
+		width: "80%",
+		borderColor: "black",
+		borderWidth: 1,
+		padding: 10,
+		marginBottom: 20,
+		borderRadius: 20,
+	},
+	buttonContainer: {
+		flexDirection: "row",
+		justifyContent: "flex-end",
+		width: "95%",
+		marginBottom: 10,
+	},
+	button: {
+		backgroundColor: "#D9D9D9",
+		padding: 3,
+		borderRadius: 20,
+		textAlign: "center",
+	},
+	cancelText: {
+		fontSize: 16,
+		fontWeight: "bold",
+		color: "Black",
+		textAlign: "center",
+	},
+	addtext: {
+		fontSize: 16,
+		fontWeight: "bold",
+		color: "white",
+	},
+	addbutton: {
+		alignItems: "flex-end",
+		width: 30,
+		height: 30,
+		justifyContent: "center",
+		paddingHorizontal: 9,
+		borderRadius: 50,
+		elevation: 3,
+		backgroundColor: "#1F51FF",
+	},
+	addButtonContainer: {
+		flexDirection: "row",
+		justifyContent: "flex-end",
+		width: "100%",
+		marginTop: -50,
+	},
+	head: {
+		marginTop: 30,
+		marginBottom: 20,
+		fontSize: 24,
+		fontWeight: "bold",
+		color: "black",
+	},
+	widgetContainer: {
+		marginBottom: 10,
+		paddingBottom: 10,
+	},
 });
 
 export default IngredientInput;
--- components/PantryIngredients.tsx
+++ components/PantryIngredients.tsx
@@ -3,23 +3,34 @@ import { Ingredient } from "./Ingredient";
 
 // This function generate a json file from a list of pantry infredients
 export const savePantryIngredients = async (ingredients: Ingredient[]) => {
-  try {
-    const jsonString = JSON.stringify(ingredients, null, 2);
-    await AsyncStorage.setItem("pantryIngredients", jsonString);
-    console.log("Ingredients saved successfully.");
-  } catch (error) {
-    console.error("Error saving ingredients:", error);
-  }
+	try {
+		const jsonString = JSON.stringify(ingredients, null, 2);
+		await AsyncStorage.setItem("pantryIngredients", jsonString);
+		console.log("Ingredients saved successfully.");
+	} catch (error) {
+		console.error("Error saving ingredients:", error);
+	}
 };
 
 // This function returns an object list of pantryIngredients from the json
 export const loadPantryIngredients = async (): Promise<Ingredient[]> => {
-  try {
-    const jsonString = await AsyncStorage.getItem("pantryIngredients");
-    if (!jsonString) return [];
-    return JSON.parse(jsonString);
-  } catch (error) {
-    console.error("Error loading ingredients:", error);
-    return [];
-  }
+	try {
+		const jsonString = await AsyncStorage.getItem("pantryIngredients");
+		if (!jsonString) return [];
+		return JSON.parse(jsonString);
+	} catch (error) {
+		console.error("Error loading ingredients:", error);
+		return [];
+	}
+};
+
+export const deleteIngredient = async (ingr: Ingredient) => {
+	try {
+		const ingredients = await loadPantryIngredients();
+		const removedIngredientList = ingredients.filter((i) => i.id !== ingr.id);
+		await savePantryIngredients(removedIngredientList);
+		//console.log(`Ingredient ${ingr.name} deleted`);
+	} catch (error) {
+		console.log(`Unable to delete ingredient ${ingr.name}: `, error);
+	}
 };
